866ff40d8798ddba6d64e25e15660276b26577bb
amiramel <amirelzam@gmail.com>
2018-09-04 14:23:58 +0300

    Add files via upload

---
+++ SeleniumLibrary/MySeleniumWebDriver/MyTestClass.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace MySeleniumWebDriver
+{
+    public class MyTestClass
+    {
+        private IWebDriver webDriver;
+        [SetUp]
+        public void InitDriver(IWebDriver webDriver)
+        {
+            webDriver = new ChromeDriver();
+        }
+        [Test]
+        public void GoToGoogle()
+        {
+            webDriver.Url = "www.google.com";
+        }
+        [TearDown]
+        public void Close()
+        {
+            webDriver.Quit();
+        }
+    }
+
+
+   
+}
