459668493194b673bf5151cc79c405b47474b4ef
amiramel <amirelzam@gmail.com>
2018-09-04 14:25:27 +0300

    Add files via upload

---
+++ SeleniumLibrary/TestClassSelenium/LoginTestClass.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using NUnit;
+using NUnit.Framework;
+
+namespace TestClassSelenium
+{
+   
+    [TestFixture]
+    public class LoginTestClass : TestBase
+    {
+        [Test]
+        public void VerifyValidLogin()
+        {
+            //Open site
+            driver.Url = "http://manage.zapbeta.co.il/web/sites-login.aspx";
+
+            //Enter Username and password
+            driver.FindElement(By.Id("iEmail")).SendKeys("amiram");
+            driver.FindElement(By.Id("iPassword")).SendKeys("amiram123");
+            //Click on Login button
+            driver.FindElement(By.Id("btnGoLogin")).Click();
+        }
+
+        [Test]
+        public void VerifyInvalidLogin()
+        {
+            //Open site
+            driver.Url = "http://manage.zapbeta.co.il/web/sites-login.aspx";
+
+            //Enter wrong Username and password 
+            driver.FindElement(By.Id("iEmail")).SendKeys("test");
+            driver.FindElement(By.Id("iPassword")).SendKeys("test");
+            //Click on Login button
+            driver.FindElement(By.Id("btnGoLogin")).Click();
+        }
+    }
+
+}
---
+++ SeleniumLibrary/TestClassSelenium/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassSelenium
+{
+    public class TestBase
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver(); // Lounches browser
+            driver.Manage().Window.Maximize(); // Maximizes browser
+            
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+
+    }
+}
